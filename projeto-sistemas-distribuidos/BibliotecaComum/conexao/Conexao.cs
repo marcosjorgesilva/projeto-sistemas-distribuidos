d213b61fd0925a05942cc1803b8da18da9bfe8a3
Pedro Santos <pedro_absantos@outlook.com>
2019-03-18 11:08:19 -0300

    Initial commit

---
+++ projeto-sistemas-distribuidos/BibliotecaComum/conexao/Conexao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaComum.conexao
+{
+    class Conexao
+    {
+        #region declaração de variáveis
+        private const string local = "DESKTOP-MHIT88S\\SQLEXPRESS";
+        private const string bancoDeDados = "MELOCHICOUT";
+        private const string usuario = "melochicout";
+        private const string senha = "123";
+        // constante local deve ser alterada para a instância local do banco(cada máquina tem a sua)
+        #endregion
+
+        // string usada para estabelecer conexão com base de dados
+        string stringConexaoSqlServer = @"Data Source=" + local + ";Initial Catalog=" + bancoDeDados +
+                                        ";UId=" + usuario + ";Password=" + senha + ";";
+
+        // classe responsável por lidar com a base de dados, uma espécie de Driver nativo da linguagem
+        public SqlConnection sqlConnection;
+
+        public void abrirConexao()
+        {
+            // instanciando uma conexão usando a string como parâmetro
+            this.sqlConnection = new SqlConnection(stringConexaoSqlServer);
+
+            // abrindo conexão com base de dados
+            this.sqlConnection.Open();
+        }
+
+        public void fecharConexao()
+        {
+            // fechando conexão
+            this.sqlConnection.Close();
+
+            // liberando memória alocada anteriormente
+            this.sqlConnection.Dispose();
+        }
+    }
+}
