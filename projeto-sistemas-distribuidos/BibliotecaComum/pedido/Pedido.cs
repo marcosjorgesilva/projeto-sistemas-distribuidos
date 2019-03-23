d6c9968e97c6350a24b98889790a24457d9383d1
Luiz <luiizhs15@gmail.com>
2019-03-23 12:32:00 -0300

    CommitPedido
    Criação de pacotes de pedido

---
+++ projeto-sistemas-distribuidos/BibliotecaComum/pedido/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaComum.pedido
+{
+    class Pedido
+    {
+        private int idPedido;
+        private string usuario;
+        private formadePagamento formadePagamento;
+        private DateTime dataPedido;
+        private DateTime dataEntrega;
+        private double valorPedido;
+
+        public int IdPedido { get => idPedido; set => idPedido = value; }
+        public string Usuario { get => usuario; set => usuario = value; }
+        public formadePagamento FormadePagamento { get => formadePagamento; set => formadePagamento = value; }
+        public DateTime DataPedido { get => dataPedido; set => dataPedido = value; }
+        public DateTime DataEntrega { get => dataEntrega; set => dataEntrega = value; }
+        public double ValorPedido { get => valorPedido; set => valorPedido = value; }
+    }
+}
