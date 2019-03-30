0ab797289316c49d5273532e05446e77b6f95b70
Marcos Jorge da Silva Filho <marcos.jorge1996@gmail.com>
2019-03-30 02:44:36 -0300

    Implementações Modulo de Produto e algumas correções das classes de categoria.

---
+++ projeto-sistemas-distribuidos/BibliotecaComum/produto/ProdutoInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaComum.produto
+{
+    interface ProdutoInterface
+    {
+        void insert(Produto produto);
+        void delete(Produto produto);
+        void update(Produto produto);
+        List<Produto> list(Produto produto);
+    }
+}
