71628148fd5e303f4eec6d45316ed27fa9d28b73
Luiz <luiizhs15@gmail.com>
2019-04-27 11:52:30 -0300

    Commit Pedido
    Atualizando.. 27.04

--- projeto-sistemas-distribuidos/BibliotecaComum/pedido/PedidoDados.cs
+++ projeto-sistemas-distribuidos/BibliotecaComum/pedido/PedidoDados.cs
@@ -1,12 +1,144 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BibliotecaComum.conexao;
+using System.Data.SqlClient;
 
 namespace BibliotecaComum.pedido
 {
-    class PedidoDados
+    public class PedidoDados : PedidoInterface, Conexao
     {
+        #region Insert
+
+        public void Create(Pedido pedido)
+        {
+            this.abrirConexao();
+            string sqlQuery = "INSERT INTO PEDIDO (idpedido, usuario, formapagamento, datapedido, dataentrega, valorpedido)";
+            sqlQuery += "VALUES (@idpedido, @usuario, @formapagamento, @datapedido, @dataentrega, @valorpedido)";
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, this.sqlConnection);
+
+            cmd.Parameters.Add("@idPedido", SqlDbType.Int);
+            cmd.Parameters["@idpedido"].Value = pedido.IdPedido;
+
+            cmd.Parameters.Add("@usuario", SqlDbType.Usuario);
+            cmd.Parameters["@usuario"].Value = pedido.Usuario;
+
+            cmd.Parameters.Add("@formapagamento", SqlDbType.FormaPagamento);
+            cmd.Parameters["@formapagamento"].Value = pedido.FormaPagamento;
+
+            cmd.Parameters.Add("@datapedido", SqlDbType.DateTime);
+            cmd.Parameters["@datapedido"].Value = pedido.DataPedido;
+
+            cmd.Parameters.Add("@dataentrega", SqlDbType.DateTime);
+            cmd.Parameters["@dataentrega"].Value = pedido.DataEntrega;
+
+            cmd.Parameters.Add("@valorpedido", SqlDbType.Decimal);
+            cmd.Parameters["@valorpedido"].Value = pedido.ValorPedido;
+
+            cmd.ExecuteNonQuery();
+
+            cmd.Dispose();
+
+            this.fecharConexao();
+        }
+        #endregion
+        #region Remove
+        public void Remove(Pedido pedido)
+        {
+            this.abrirConexao();
+            string sqlQuery = "delete from pedido";
+            sqlQuery += "where idpedido = @idpedido";
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, this.sqlConnection);
+
+            cmd.Parameters.Add("@idPedido", SqlDbType.Int);
+            cmd.Parameters["@idpedido"].Value = pedido.IdPedido;
+
+            cmd.ExecuteNonQuery();
+
+            cmd.Dispose();
+
+            this.fecharConexao();
+        }
+        #endregion
+        #region Update
+        public void Update(Pedido pedido)
+        {
+            this.abrirConexao();
+            string sqlQuery = "update pedido set";
+            sqlQuery += "usuario = @usuario";
+            sqlQuery += "where idpedido = @idpedido";
+            
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, this.sqlConnection);
+
+            cmd.Parameters.Add("@idPedido", SqlDbType.Int);
+            cmd.Parameters["@idpedido"].Value = pedido.IdPedido;
+
+            cmd.Parameters.Add("@usuario", SqlDbType.Usuario);
+            cmd.Parameters["@usuario"].Value = pedido.Usuario;
+
+            cmd.ExecuteNonQuery();
+
+            cmd.Dispose();
+
+            this.fecharConexao();
+        }
+        #endregion
+        #region Select
+        public List<Pedido> Select(Pedido filtro)
+        {
+            List<Pedido> retorno = new List<Pedido>();
+            this.abrirConexao();
+            string sqlQuery = "SELECT (idpedido, usuario)";
+            sqlQuery += "FROM Pedido";
+            sqlQuery += "Where pedido = @idpedido";
+
+            if (filtro.IdPedido > 0)
+            {
+                sqlQuery += " and idpedido = @idpedido";
+            }
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, this.sqlConnection);
+
+            if (filtro.IdPedido > 0)
+            {
+                cmd.Parameters.Add("@idpedido", SqlDbType.Int);
+                cmd.Parameters["@idpedido"].Value = filtro.IdPedido;
+            }
+   
+            if (filtro.Usuario != null && filtro.Usuario.Trim().Equals("") == false)
+            {
+                cmd.Parameters.Add("@usuario", SqlDbType.VarChar);
+                cmd.Parameters["@usuario"].Value = "%" + filtro.Usuario + "%";
+
+            }
+
+            SqlDataReader DbReader = cmd.ExecuteReader();
+            
+            while (DbReader.Read())
+            {
+                Pedido pedido = new Pedido();
+                
+                pedido.IdPedido = DbReader.GetInt32(DbReader.GetOrdinal("idpedido"));
+                pedido.Usuario = DbReader.GetString(DbReader.GetOrdinal("usuario"));
+                retorno.Add(pedido);
+            }
+            
+            DbReader.Close();
+
+            cmd.ExecuteNonQuery();
+
+            cmd.Dispose();
+
+            this.fecharConexao();
+
+        }
+        #endregion
+
     }
 }
--- projeto-sistemas-distribuidos/BibliotecaComum/pedido/PedidoNegocio.cs
+++ projeto-sistemas-distribuidos/BibliotecaComum/pedido/PedidoNegocio.cs
@@ -6,7 +6,79 @@ using System.Threading.Tasks;
 
 namespace BibliotecaComum.pedido
 {
-    class PedidoNegocio
+    public class PedidoNegocio : PedidoInterface
     {
+        #region Remover
+        public void Remove(Pedido pedido)
+        {
+            if(pedido == null)
+            {
+                throw new Exception("");
+            }
+            if(pedido.IdPedido <=0 )
+            {
+                throw new Exception("id do pedido tem que ser maior que zero");
+               
+            }
+            Pedido p = new Pedido();
+            p.IdPedido = pedido.IdPedido;
+            PedidoDados dados = new PedidoDados();
+            if(dados.Select(p).Count <= 0)
+            {
+                throw new Exception("O id não se encontra cadastrado");
+            }
+            dados.Remove(pedido);
+
+        }
+        #endregion
+        #region Create
+        public void Create(Pedido pedido)
+        {
+            if(pedido == null)
+            {
+                throw new Exception("");
+            }
+            if(pedido.IdPedido <= 0)
+            {
+                throw new Exception("id invalido");
+            }
+            PedidoDados dados = new PedidoDados();
+            Pedido p = new Pedido();
+            p.IdPedido = pedido.IdPedido;
+
+            if(dados.Select(p).Count > 0)
+            {
+                throw new Exception("Matricula cadastrada");
+            }
+            dados.Create(pedido);
+        }
+        #endregion
+        #region Select
+        public List<Pedido> Select(Pedido filtro)
+        {
+            return new PedidoDados().Select(filtro);
+        }
+        #endregion
+        public void Update(Pedido pedido)
+        {
+            if (pedido == null)
+            {
+                throw new Exception("");
+            }
+            if (pedido.IdPedido <= 0)
+            {
+                throw new Exception("id invalido");
+            }
+            PedidoDados dados = new PedidoDados();
+            Pedido p = new Pedido();
+            p.IdPedido = pedido.IdPedido;
+
+            if (dados.Select(p).Count > 0)
+            {
+                throw new Exception("Matricula cadastrada");
+            }
+            dados.Update(pedido);
+
+        }    
     }
 }
