eb59a526937b2d9ed777139146fa389b1640a24e
João Gilberto <joaogilberto_ns@yahoo.com.br>
2019-04-30 21:06:25 -0300

    Correção das classes

--- projeto-sistemas-distribuidos/BibliotecaComum/forma_pagamento/FormaPagamentoNegocio.cs
+++ projeto-sistemas-distribuidos/BibliotecaComum/forma_pagamento/FormaPagamentoNegocio.cs
@@ -1,36 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
-using System.Data;
 
 namespace BibliotecaComum.forma_pagamento
 {
     public class FormaPagamentoNegocio : FormaPagamentoInterface
     {
-        FormaPagamentoDados formaPagamentoDados = new FormaPagamentoDados();
-
-        public void Delete(FormaPagamento formaPagamento)
-        {
-            formaPagamentoDados.Delete(formaPagamento);
-        }
-
+        
+        #region validação Insert
         public void Insert(FormaPagamento formaPagamento)
         {
-            formaPagamentoDados.Insert(formaPagamento);
+            #region validações
+
+            #region validação de objeto
+            isformaPagamentoValida(formaPagamento);
+            #endregion
+
+            #region validação de Id
+            isIdFormaPagamentoValida(formaPagamento);
+            #endregion
+
+            #region validações de descrição
+            isDescricaoValida(formaPagamento);
+            #endregion
+
+            #region validações de tipo
+            isTipoValido(formaPagamento);
+            #endregion
+
+            #endregion
+
+            // chamada ao DAO
+            new FormaPagamentoDados().Insert(formaPagamento);
         }
+        #endregion
 
-        public List<FormaPagamento> Select(FormaPagamento filtro)
-        {
-           return formaPagamentoDados.Select(filtro);
-
-        }
-
+        #region validação Update
         public void Update(FormaPagamento formaPagamento)
         {
-            formaPagamentoDados.Update(formaPagamento);
+            new FormaPagamentoDados().Update(formaPagamento);
         }
+        #endregion
+
+        #region validação Delete
+        public void Delete(FormaPagamento formaPagamento)
+        {            
+            new FormaPagamentoDados().Delete(formaPagamento);
+        }
+        #endregion
+
+        #region validação Select
+        public List<FormaPagamento> Select(FormaPagamento filtro)
+        {
+            return new FormaPagamentoDados().Select(filtro);
+        }
+        #endregion                                   
+
+        #region métodos para uso interno da classe
+        private static bool isformaPagamentoValida(FormaPagamento formaPagamento)
+        {
+            if (formaPagamento.Equals(null))
+                throw new Exception("Nulo: instanciar o forma de pagamento!");
+            return true;
+        }
+
+        private static bool isIdFormaPagamentoValida(FormaPagamento formaPagamento)
+        {
+            if (formaPagamento.IdFormaPagamento <= 0)
+                throw new Exception("Código Inválido: o código da forma de pagamento precisa ser maior que zero!");
+            return true;
+        }
+
+        private static bool isDescricaoValida(FormaPagamento formaPagamento)
+        {
+            if (formaPagamento.DescricaoFp == null)
+                throw new Exception("Nulo: Informar a forma de pagamento!");
+            if (formaPagamento.DescricaoFp.Length > 50)
+                throw new Exception("A descrição da forma de pagamento não pode exceder 50 caracteres");
+
+            return true;
+        }
+
+        private static bool isTipoValido(FormaPagamento formaPagamento)
+        {
+            if (formaPagamento.TipoFp == null)
+                throw new Exception("Nulo: Informar o tipo de forma de pagamento!");
+            if (formaPagamento.TipoFp.Length > 50)
+                throw new Exception("O tipo de forma pagamento não pode exceder 50 caracteres");
+
+            return true;
+        }
+        #endregion
+
     }
 }
--- projeto-sistemas-distribuidos/BibliotecaComum/itens_pedido/ItensPedidoNegocio.cs
+++ projeto-sistemas-distribuidos/BibliotecaComum/itens_pedido/ItensPedidoNegocio.cs
@@ -1,31 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BibliotecaComum.itens_pedido
 {
-    class ItensPedidoNegocio : ItensPedidoInterface
+    public class ItensPedidoNegocio : ItensPedidoInterface
     {
-        public void Delete(ItensPedido itensPedido)
-        {
-            new ItensPedidoDados().Delete(itensPedido);
-        }
-
+        #region validação Insert
         public void Insert(ItensPedido itensPedido)
         {
+            #region validações
+
+            #region validação de objeto
+            isItensPedidoValido(itensPedido);
+            #endregion
+
+            #region validação de Id do Pedido
+            isIdPedidoValido(itensPedido);
+            #endregion
+
+            #region validação de Id do Produto
+            isIdProdutoValido(itensPedido);
+            #endregion
+            
+            #region validação de quantidade
+            isQuantidadeValida(itensPedido);
+            #endregion
+
+            #region validação de valor dos itens
+            isValorItemValido(itensPedido);
+            #endregion
+
+            #region validação de descrição
+            isDescricaoValida(itensPedido);
+            #endregion
+
+            #endregion
+
+
+            // chamada ao DAO
             new ItensPedidoDados().Insert(itensPedido);
-        }
 
-        public List<ItensPedido> Select(ItensPedido filtro)
-        {
-            return new ItensPedidoDados().Select(filtro);
         }
+        #endregion
 
+        #region validação Update
         public void Update(ItensPedido itensPedido)
         {
             new ItensPedidoDados().Update(itensPedido);
         }
+        #endregion
+
+        #region validação Delete
+        public void Delete(ItensPedido itensPedido)
+        {
+            new ItensPedidoDados().Delete(itensPedido);
+        }
+        #endregion
+
+        #region validação Select
+        public List<ItensPedido> Select(ItensPedido filtro)
+        {
+            return new ItensPedidoDados().Select(filtro);
+        }
+        #endregion
+
+        #region métodos para uso interno da classe
+        private static bool isItensPedidoValido(ItensPedido itensPedido)
+        {
+            if (itensPedido.Equals(null))
+                throw new Exception("Nulo: instanciar itens pedido!");
+            return true;
+        }
+
+        private static bool isIdPedidoValido(ItensPedido itensPedido)
+        {
+            if (itensPedido.IdPedido <= 0)
+                throw new Exception("Código Inválido: o código do pedido precisa ser maior que zero!");
+            return true;
+        }
+
+        private static bool isIdProdutoValido(ItensPedido itensPedido)
+        {
+            if (itensPedido.IdProduto <= 0)
+                throw new Exception("Código Inválido: o código do produto precisa ser maior que zero!");
+            return true;
+        }
+
+        private static bool isQuantidadeValida(ItensPedido itensPedido)
+        {
+            if (itensPedido.Quantidade <= 0)
+                throw new Exception("Quantidade Inválida: a quantidade de produtos precisa ser maior que zero!");
+            return true;
+        }
+
+        private static bool isValorItemValido(ItensPedido itensPedido)
+        {
+            if (itensPedido.ValorItens <= 0)
+                throw new Exception("Valor Inválido: o valor dos produtos precisa ser maior que zero!");
+            return true;
+        }
+
+        private static bool isDescricaoValida(ItensPedido itensPedido)
+        {
+            if (itensPedido.DescricaoIp.Length > 150)
+                throw new Exception("A descrição do pedido não pode exceder 150 caracteres");
+
+            return true;
+        }
+
+        #endregion
     }
 }
